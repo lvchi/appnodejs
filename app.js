d0bdd8782880e07439024e03d927425497f67838
chilv <chilv@gmail.com>
2022-05-05 21:15:32 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!!!!'))
-app.listen(3000, () => console.log('Check log service!!!'))
+app.get('/', (req, res) => res.send('codestar devops!!!'))
+app.listen(3000, () => console.log('Check container ecs!!!'))
